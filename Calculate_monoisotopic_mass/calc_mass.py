82d24bcc2ab6626179347889cbd42344ce36379a
Vijay Lakhujani <lakhujanivijay@gmail.com>
2016-08-30 16:57:57 +0530

    Update calc_mass.py

--- Calculate_monoisotopic_mass/calc_mass.py
+++ Calculate_monoisotopic_mass/calc_mass.py
@@ -1,8 +1,9 @@
+
 #!/usr/bin/python3
 __author__ = "Vijay Lakhujani"
 __credits__ = ["Vijay Lakhujani"]
 __version__ = "1.0"
-__email__ = "lakhjanivijay@gmail.com"
+__email__ = "lakhujanivijay@gmail.com"
 __status__ = "done"
 
 import re 
