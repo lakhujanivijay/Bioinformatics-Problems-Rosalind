bec3fcadad99aab7cf4586aa4e99e0dc8442c7d6
Vijay Lakhujani <lakhujanivijay@gmail.com>
2016-08-30 16:58:42 +0530

    Update complement_dna.py

--- DNA_complement/complement_dna.py
+++ DNA_complement/complement_dna.py
@@ -1,4 +1,10 @@
-__author__'"Vijay Lakhujani"
+
+#!/usr/bin/python3
+__author__ = "Vijay Lakhujani"
+__credits__ = ["Vijay Lakhujani"]
+__version__ = "1.0"
+__email__ = "lakhujanivijay@gmail.com"
+__status__ = "done"
 
 def reverse_complement(seq):
     '''
